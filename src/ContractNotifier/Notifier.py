721904106664bbbff3ec34400df2d4570f49362a
Joran Honig <info@joranhonig.nl>
2018-03-21 20:14:04 +0100

    Add callback to notifier

--- src/ContractNotifier/Notifier.py
+++ src/ContractNotifier/Notifier.py
@@ -1,4 +1,7 @@
 class Notifier:
 
-    def __initialize__(self):
-        pass
+    def __initialize__(self, callback):
+        self.callback = callback
+
+    def encounter(self, address, source=None):
+        self.callback(address, self, source)
