aa98a53ce81c9470ff05a37983960927115feeaa
Joran Honig <info@joranhonig.nl>
2018-03-22 15:36:47 +0100

    Add logging and comments to analysis runner

--- src/AnalysisRunner/AnalysisRunner.py
+++ src/AnalysisRunner/AnalysisRunner.py
@@ -1,3 +1,6 @@
+import logging
+
+
 class AnalysisRunner:
 
     def __init__(self, analysis_functions=[]):
@@ -5,6 +8,7 @@ class AnalysisRunner:
         Constructor for AnalysisRunner
         :param analysis_functions: analysis functions that should be ran
         """
+        logging.debug("Initializing analysis runner")
         self.functions = analysis_functions
 
     def add_analysis_function(self, f):
@@ -12,12 +16,22 @@ class AnalysisRunner:
         Add a function to analyse contracts
         :param f: function to add
         """
+        logging.debug("Added analysis function")
         self.functions += f
 
     def analyse(self, address):
+        """
+        Analyse contract
+        :param address: address of contract to analyse
+        :return: findings
+        """
+        logging.info("Starting analysis of contract at address: {}".format(address))
+
         findings = []
         # run analysis
         for f in self.functions:
             findings += f(address)
+
+        logging.info("During analysis we found {} issues".format(len(findings)))
 
         return findings
