8fd84fdceb1612fae6f57f684ca56df2f0e65efd
Joran Honig <info@joranhonig.nl>
2018-03-22 13:55:46 +0100

    Add comments and remove interval from analysisManager

--- src/AnalysisManager.py
+++ src/AnalysisManager.py
@@ -4,20 +4,30 @@ import logging
 
 class AnalysisManager:
 
-    def __init__(self, notifier, reporter, interval):
+    def __init__(self, notifier, reporter, runner):
+        """
+        Constructor for Analysismanager
+        :param notifier: Notifier object which finds new contracts
+        :param reporter: Reporter object used to output findings
+        :param runner: Runner objects which analyses contracts and constructs findings
+        """
         self.notifier = notifier
         self.reporter = reporter
-        self.interval = interval
+        self.runner = runner
 
         self.notifier.callback = self._contract_found
 
-        # Start looping
-        self.loop()
-
-    def loop(self):
-        time.sleep(self.interval)
+        # Start scanning
         self.notifier.scan()
 
     def _contract_found(self, contract_address, source):
+        """
+        Analyse contract and report findings
+        :param contract_address: Address of contract to analyse
+        :param source: source name of notifier
+        """
         logging.info("Analysis manager got contract with address {} from {}", contract_address, source)
-        
+        findings = self.runner.analyse(contract_address)
+        # Report findings
+        for finding in findings:
+            self.reporter.add_finding(finding)
