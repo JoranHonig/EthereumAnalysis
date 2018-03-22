4671021d9e6a95f9c415459d88666c35f2dfbfd9
Joran Honig <info@joranhonig.nl>
2018-03-22 13:18:18 +0100

    Make functions private Add examining functions

--- src/ContractNotifier/LocalNotifier.py
+++ src/ContractNotifier/LocalNotifier.py
@@ -17,7 +17,30 @@ class LocalNotifier(Notifier):
 
         logging.info("The ethereum client is currently at block {}", self.current_block)
 
-
-    # Private functions
-    def new_blocks(self):
+    def _new_blocks(self):
         return self.rpc_client.eth_blockNumber() - self.current_block
+
+    def _examine_block(self, number):
+        # Gets the block by number, true indicates that we want the entire transactions instead of only hashes
+        block = self.rpc_client.eth_getBlockByNumber(number, True)
+
+        logging.info("Examining block with number {}", number)
+
+        transactions = block.transactions
+
+    def _examine_transaction(self, transaction_object):
+        to = transaction_object.to
+
+        if to is not None:
+            logging.debug("Transaction with hash {} is not a contract creating transaction", transaction_object.hash)
+            return
+
+        logging.info("Found contract creating transaction with hash {}", transaction_object.hash)
+
+        transaction_receipt = self.rpc_client.eth_getTransactionReceipt(transaction_object.hash)
+
+        contract_address = transaction_receipt.contractAddress
+
+        logging.info("Found new contract with address {}")
+
+        self.encounter(contract_address, "LocalNotifier")
