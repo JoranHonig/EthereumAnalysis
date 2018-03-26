ca712a0e18c9b79fe171564b8fa9aa668b2bb3e8
Joran Honig <info@joranhonig.nl>
2018-03-26 14:48:05 +0200

    add option for local use

--- analyser.py
+++ analyser.py
@@ -11,5 +11,7 @@ rpc_settings.add_argument('-i', action='store_true', help="Use infura network")
 
 parser.add_argument('-f', action='store_true', help='Start analysis from genesis block')
 
+parser.add_argument('-l', action='store_true', help='Use local modules for notification and reporting')
+
 args = parser.parse_args()
 
