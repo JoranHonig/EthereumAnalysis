b284242fdac26f4e961e284d8d0ec425a161ad2a
Joran Honig <joran.honig@gmail.com>
2018-03-28 14:14:01 +0200

    Update finding to include pc and severity attribute

--- EthereumAnalysis/Report/Finding.py
+++ EthereumAnalysis/Report/Finding.py
@@ -1,7 +1,9 @@
 class Finding:
 
-    def __init__(self, source, name, description, address):
+    def __init__(self, source, name, description=None, address=None, pc=None, severity=None):
         self.source = source
         self.name = name
         self.description = description
         self.address = address
+        self.pc = pc
+        self.severity = severity
